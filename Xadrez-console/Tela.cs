1e2a2ae0c65929d151662a2b51bf4a82d9d1dc81
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-01-19 16:18:00 -0300

    Começando a implementar o metodo de impressão do tabuleiro.

---
+++ Xadrez-console/Tela.cs
@@ -0,0 +1,27 @@
+﻿using tabuleiro;
+
+namespace Xadrez_console
+{
+    class Tela
+    {
+        public static void ImprimirTabuleiro(Tabuleiro tab)
+        {
+            for(int i = 0; i < tab.Linhas; i++)
+            {
+                for(int j = 0; i <tab.Colunas; j++)
+                {
+                    Console.WriteLine(tab.peca(i, j) + " ");
+                    if (tab.peca(i,j) == null)
+                    {
+                        Console.WriteLine("- ");
+                    }
+                    else
+                    {
+                        Console.WriteLine(tab.peca(i, j) + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
