8c7070eb39a443e2e3ea88a9c8b03188afbd0d60
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-01-21 22:35:26 -0300

    Criando a classe para auxilio na posicacao do tabuleiro.

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -7,21 +7,11 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            Console.WriteLine(pos);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
-                Tela.imprimirTabuleiro(tab);
-                Console.ReadLine();
+            Console.WriteLine(pos.toPosicao());
 
-            }
-            catch (TabuleiroException e) 
-            {
-                Console.WriteLine(e.Message);
-            }
         }
     }
 }
